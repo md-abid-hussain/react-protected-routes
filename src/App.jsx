9d07559a13571208240a932f5304ebfba6570ee2
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-22 13:23:13 +0530

    added react router to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-
+import Layout from './components/Layout'
+import { Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+import LinkPage from './components/LinkPage'
+import Home from './components/Home'
+import Admin from './components/Admin'
+import Editor from './components/Editor'
+import Missing from './components/Missing'
+import RequireAuth from './components/RequireAuth'
+import Lounge from './components/Lounge'
+import Unauthorized from './components/Unauthorized'
 
 function App() {
-
-
   return (
-    <main>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* Public Links */}
+        <Route path='register' element={<Register />} />
+        <Route path='login' element={<Login />} />
+        <Route path='linkpage' element={<LinkPage />} />
+        <Route path='unauthorized' element={<Unauthorized />} />
 
-    </main>
+        {/* Protected links */}
+        <Route element={<RequireAuth allowedRoles={[2001]} />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[5150]} />}>
+          <Route path='admin' element={<Admin />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[1984]} />}>
+          <Route path='editor' element={<Editor />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[1984, 5150]} />}>
+          <Route path='lounge' element={<Lounge />} />
+        </Route>
+
+        <Route path='*' element={<Missing />} />
+
+      </Route>
+    </Routes>
   )
 }
 
