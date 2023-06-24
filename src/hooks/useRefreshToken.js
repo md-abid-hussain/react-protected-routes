8153411037fc3626b14e0d7237749e2eec170d53
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-24 10:16:53 +0530

    created hooks

---
+++ src/hooks/useRefreshToken.js
@@ -0,0 +1,20 @@
+import useAuth from './useAuth'
+import axios from '../api/axios'
+
+const useRefreshToken = () => {
+    const { setAuth } = useAuth();
+    const refresh = async () => {
+        const response = await axios.get('/refresh', {
+            withCredentials: true
+        })
+        setAuth(prev => {
+            console.log(prev.accessToken)
+            console.log(response.data.accessToken)
+            return { ...prev, accessToken: response.data.accessToken }
+        })
+        return response.data.accessToken
+    }
+    return refresh
+}
+
+export default useRefreshToken
