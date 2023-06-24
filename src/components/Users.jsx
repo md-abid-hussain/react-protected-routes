551706a5e2aa09de309bf3d5fbb1bbcd87c25d45
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-24 10:17:32 +0530

    created Users component

---
+++ src/components/Users.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect, useRef } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
+
+const Users = () => {
+  const effectRef = useRef(false);
+
+  const [users, setUsers] = useState();
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+        const response = await axiosPrivate.get('/user', {
+          signal: controller.signal
+        })
+        console.log(response.data)
+        isMounted && setUsers(response.data)
+      } catch (err) {
+        console.error(err)
+        navigate('/login', { state: { from: location }, replace: true })
+      }
+    }
+    if (effectRef.current) {
+      getUsers()
+    }
+    return () => {
+      isMounted = false;
+      controller.abort();
+      effectRef.current = true;
+    }
+
+
+  }, [])
+
+  return (
+    <article>
+      <h2>Users List</h2>
+      {users?.length
+        ? (
+          <ul>
+            {users.map((user, i) => <li key={i}>{user?.username}</li>)}
+          </ul>
+        )
+        :
+        <p>No users to display</p>
+      }
+    </article>
+  )
+}
+
+export default Users
